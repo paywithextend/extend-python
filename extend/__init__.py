74418e51846af67cd9a262595116b30b0778a76c
John Busby <johncbusby@gmail.com>
2025-04-09 08:47:25 -0700

    refactor: centralize versioning

--- extend/__init__.py
+++ extend/__init__.py
@@ -3,9 +3,10 @@ Python client for the Extend API.
 """
 
 from extend.models import VirtualCard, Transaction, RecurrenceConfig
+from .__version__ import __version__ as _version
 from .extend import ExtendClient
 
-__version__ = "1.1.0"
+__version__ = _version
 
 __all__ = [
     "ExtendClient",
